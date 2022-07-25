5a3091844079fd8b33a3996f852051b577df58d2
Adarsh Kumar <adarsh.asm@gmail.com>
2022-07-26 00:33:54 +0530

    challenge #2

--- challenge2/azure/src/azurenetworkinfo.ps1
+++ challenge2/azure/src/azurenetworkinfo.ps1
@@ -1,1 +1,3 @@
+#! /bin/bash
+
 curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/network?api-version=2017-08-01"
--- challenge2/azure/src/azuretrackingvm.ps1
+++ challenge2/azure/src/azuretrackingvm.ps1
@@ -1,1 +1,3 @@
+#! /bin/bash
+
 curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/compute/vmId?api-version=2017-08-01&format=text"
